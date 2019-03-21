176bd6947760ae13a0eed56dccd86044f889a052
Sumedha Gamage <sumedhagamage@gmail.com>
2019-03-20 21:36:06 -0500

    change to setup

--- setup.sh
+++ setup.sh
@@ -5,13 +5,13 @@ if [[ $? != 0 ]];
 then
     yum install docker
 fi
-service docker start
-docker swarm init
-docker stack deploy -c stack.yml mongo
 #pass varko35
 #user root
 #45.79.152.69
 
 yum install maven -y
 mvn -DskipTests clean package docker:build
-docker run -d -p 80:8080 --link mongo sgamage/api
+
+service docker start
+docker swarm init
+docker stack deploy -c stack.yml conference

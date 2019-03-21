64c13908e11e10662eb8081011c7c9eb49b4b538
Sumedha Gamage <sumedhagamage@gmail.com>
2019-03-20 22:37:22 -0500

    setup change

--- setup.sh
+++ setup.sh
@@ -4,15 +4,24 @@ docker_bin=$(which docker)
 if [[ $? != 0 ]];
 then
     yum install docker
+    service docker start
+else
+    docker kill $(docker ps -q)
 fi
 #pass varko35
 #user root
 #45.79.152.69
+docker-compose_bin=$(which /usr/local/bin/docker-compose)
+if [[ $? != 0 ]];
+then
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+fi
 
-yum install maven -y
+mvn_bin=$(which mvn)
+if [[ $? != 0 ]];
+then
+    yum install maven -y
+fi
 mvn -DskipTests clean package docker:build
-
-service docker start
-docker swam leave
-docker swarm init
-docker stack deploy -c stack.yml conference
+/usr/local/bin/docker-compose up
